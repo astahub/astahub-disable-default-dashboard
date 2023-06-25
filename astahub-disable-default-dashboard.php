969726475dc2d43fa87b9234b2447f2b273ff71e
Haris Ainur Rozak <harisrozak@gmail.com>
2023-06-25 23:39:36 +0700

    added health status widget to remove

--- astahub-disable-default-dashboard.php
+++ astahub-disable-default-dashboard.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/astahub/astahub-disable-default-dashboard
 Description: Disable build in dashboard items widget.
 Author: harisrozak
 Author URI: https://github.com/harisrozak
-Version: 0.1
+Version: 1.1
 Text Domain: astahub-disable-dashboard-items
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
@@ -21,5 +21,6 @@ function astahub_disable_default_dashboard_items() {
     remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
     remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
     remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal'); // since 3.8
+    remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal'); // since 5.0
 }
